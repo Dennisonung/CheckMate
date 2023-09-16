26d0f0f3a7ec63a4cc7d0a22743f8a81187962b6
Dennisonung <Dennisonung@gmail.com>
2023-09-16 02:45:09 -0400

    API

--- API-v1/index.js
+++ API-v1/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sha512 = require("js-sha512").sha512;
 const fs = require("fs");
 const path = require("path");
 const app = express();
@@ -6,13 +7,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 global.api = "/api/v1";
 global.Config = JSON.parse(fs.readFileSync(__dirname + '/config/config.json'));
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://checkmateAdmin:${Config.mongoosePassword}@cluster0.wag2biu.mongodb.net/`)
+	.then(() => console.log('Connected to MongoDB.'));
 global.forbidden = ["null","forbidden","fs"];
-
-let cores = fs.readdirSync("./" + Config.API_Version +"/modules");
-for (const i in cores) {
-	let file = cores[i];
+const ForbiddenFiles = [];
+let modules = fs.readdirSync("./modules");
+for (const i in modules) {
+	let file = modules[i];
 	if (!ForbiddenFiles.includes(file)) {
-		const userModule = require("./" + Config.API_Version +"/modules/" + file);
+		const userModule = require("./modules/" + file);
 		console.debug("Init module " + file);
 		userModule.run(app, path, fs, sha512);
 	};
---
+++ API-v1/modules/group.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'group',
+    description: 'group management module',
+    ErrorCodeRange: 100,
+    run: async function (app, path, fs, sha512) {
+    
+    }
+}    
