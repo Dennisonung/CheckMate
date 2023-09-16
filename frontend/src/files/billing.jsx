6605b3bb876fcb47af71c082e75097de45dc09eb
joshuazhou744 <joshuazhou744@gmail.com>
2023-09-16 08:59:09 -0400

    Rough draft billing page

--- frontend/src/files/billing.jsx
+++ frontend/src/files/billing.jsx
@@ -1,13 +1,63 @@
 import ReactDOM from 'react-dom/client'
 import Navbar from './navbar'
 import './billing.css'
+import { useState } from 'react'
 
 const Billing = () => {
+
+    const [total, setTotal] = useState('')
+
+    const divFactor = 2;
+
+    const handleChange = e => {
+        setTotal(e.target.value);
+    }
+
+    const handleClick = e => {
+        e.preventDefault();
+        console.log(total)
+    }
+
+
     return (
         <div>
-            <h1>New Bill</h1>
+            <div className="containerDiv">
+                <h1>New Bill</h1>
 
-            <h3 className='bold'>*** Be sure all members splitting this bill</h3>
+                <h3 className='bold'>*** Be sure all members splitting this bill have been selected on the Group page</h3>
+
+                <div className="billDiv">
+
+                    <div className="totalInput">
+                        <label htmlFor="total">Total Bill</label>
+                        <input type="text" id="total" onChange={handleChange} placeholder='$'/>
+                        <button className="button" onClick={handleClick}>Confirm</button>
+                    </div>
+
+                    <div className="payeeList">
+                        <h2>Payee List:</h2>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div className="container">
+
+                    <div className="summary">
+                    <h3>${total} split {divFactor} ways, each party pays ${Math.ceil(total/divFactor)}</h3>
+                    </div>
+
+                    <div className="buttonDiv">
+                        <button className="button">
+                            Send
+                        </button>
+                    </div>
+
+                </div>
+            </div>
 
             <Navbar />
         </div>
