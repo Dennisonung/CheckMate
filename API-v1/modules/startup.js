7dcb9a08fd51a285ab31954a8458943bc256902a
Dennisonung <Dennisonung@gmail.com>
2023-09-16 11:12:28 -0400

    fixed users.js

--- API-v1/modules/startup.js
+++ API-v1/modules/startup.js
@@ -4,10 +4,15 @@ module.exports = {
     ErrorCodeRange: "010",
     run: async function (fs,path) {   
         // make directory "nonpersistent" in the root directory
-        
+
         if (!fs.existsSync(path.join(__dirname, "../nonpersistent"))) {
             fs.mkdirSync(path.join(__dirname, "../nonpersistent"));
             fs.mkdirSync(path.join(__dirname, "../nonpersistent/ActiveSessions"));
+        } else {
+            //delete everything in activesessions
+            fs.readdirSync(path.join(__dirname, "../nonpersistent/ActiveSessions")).forEach(file => {
+                fs.unlinkSync(path.join(__dirname, "../nonpersistent/ActiveSessions", file));
+            });
         }
         if (!fs.existsSync(path.join(__dirname, "../persistent"))) {
             await fs.mkdirSync(path.join(__dirname, "../persistent"));
